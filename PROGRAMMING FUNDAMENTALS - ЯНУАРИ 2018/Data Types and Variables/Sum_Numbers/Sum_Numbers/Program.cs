fbf053ba4424ffb2b40310e934face2d4df3b8bc
Mixail <gambler4o0o0@gmail.com>
2020-04-05 22:21:05 +0300

    Learn C#

---
+++ PROGRAMMING FUNDAMENTALS - ЯНУАРИ 2018/Data Types and Variables/Sum_Numbers/Sum_Numbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 5;
+            int number2 = 10;
+            int sum = number1 + number2;
+            Console.WriteLine(sum);
+        }
+    }
+}
