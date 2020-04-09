241092d3d354f523e2999a61f782c0fb0b090b4e
Mixail <gambler4o0o0@gmail.com>
2020-04-09 22:49:26 +0300

    Learn C#

---
+++ Programming Fundamentals - Януари 2017/Data Type - Numeral Types and Type Conversion/Circle Perimetar/Circle Perimetar/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Circle_Perimetar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:f12}", 2 * Math.PI * r);
+        }
+    }
+}
