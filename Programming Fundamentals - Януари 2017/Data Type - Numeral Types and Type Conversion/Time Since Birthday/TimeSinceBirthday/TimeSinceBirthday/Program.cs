37e66f6b77c80c2605140924fb0e58404704985b
Mixail <gambler4o0o0@gmail.com>
2020-04-09 21:03:31 +0300

    Learn C#

---
+++ Programming Fundamentals - Януари 2017/Data Type - Numeral Types and Type Conversion/Time Since Birthday/TimeSinceBirthday/TimeSinceBirthday/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TimeSinceBirthday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Years - ");
+            byte years = byte.Parse(Console.ReadLine());
+            int days = years * 365;
+            int hours = days * 24;
+            int minutes = hours * 60;
+            Console.WriteLine("{0} years = {1} days = {2} hours = {3} minutes.",years,days,hours,minutes);
+        }
+    }
+}
