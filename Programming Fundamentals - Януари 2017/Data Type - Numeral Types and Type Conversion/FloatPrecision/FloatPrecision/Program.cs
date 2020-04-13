de4f1660b5da8e1b9e753cd15a78d41c020fde8f
Mixail <gambler4o0o0@gmail.com>
2020-04-13 21:26:19 +0300

    Learn C#

---
+++ Programming Fundamentals - Януари 2017/Data Type - Numeral Types and Type Conversion/FloatPrecision/FloatPrecision/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FloatPrecision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 1.0F;
+            double b = 0.33F;
+            double sum = 1.33F;
+
+            Console.WriteLine((a+b) == sum);
+        }
+    }
+}
