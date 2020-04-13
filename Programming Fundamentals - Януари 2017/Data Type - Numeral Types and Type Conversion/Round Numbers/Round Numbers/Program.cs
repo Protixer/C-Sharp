d086f82b3f5982dafb9246daba5828225118f5f4
Mixail <gambler4o0o0@gmail.com>
2020-04-13 21:16:04 +0300

    Learn C#

---
+++ Programming Fundamentals - Януари 2017/Data Type - Numeral Types and Type Conversion/Round Numbers/Round Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Round_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 2.5455;
+            Console.WriteLine(Math.Round(a));
+            Console.WriteLine(Math.Round(a,2));
+            Console.WriteLine(Math.Ceiling(a));
+            Console.WriteLine(Math.Floor(a));
+        }
+    }
+}
