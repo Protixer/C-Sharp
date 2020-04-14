9059d0912047342931c9a58e2cb815328d60bf6a
Mixail <gambler4o0o0@gmail.com>
2020-04-14 21:32:22 +0300

    Learn C#

---
+++ Programming Fundamentals - Януари 2017/Data Type - Numeral Types and Type Conversion/Transport/Transport/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Transport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int courses = (int)Math.Ceiling((double)n / (4 + 8 + 12));
+            Console.WriteLine(courses) ;
+
+        }
+    }
+}
